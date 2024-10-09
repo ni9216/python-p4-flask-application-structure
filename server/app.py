c201f0d8c97a9a9569f23da58d9af8e04fd3f7ff
ni9216 <nicholasliech851@gmail.com>
2024-10-09 09:40:57 +0300

    data

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# Step 2: Routing Views
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+# Step 3: Variable Routes
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Step 4: Flask Development Web Server
+if __name__ == '__main__':
+    app.run(debug=True,port=5555)
